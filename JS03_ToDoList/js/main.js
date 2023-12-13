68832129a238f691555c1592b501dd44bcdef580
pinowrk <bourgogne.pino.crawler@gmail.com>
2023-12-14 00:39:15 +0900

    課題修正 2023-12-14 00:39

--- JS03_ToDoList/js/main.js
+++ JS03_ToDoList/js/main.js
@@ -1,6 +1,7 @@
-// DOM要素参照elmBuzz
+// DOM要素参照
 const elmTxtTask = document.getElementById('txtTask');
 const elmTblToDo = document.getElementById('tblToDo');
+const elmTblTask = document.getElementById('tblTask');
 
 //
 // ------------------------------
@@ -21,24 +22,28 @@ btnAdd.addEventListener(
         id: allTasks.length,
         com: txtTask,
         sta: '作業中',
-        del: '削除', // 無くてもよい？？？ 今後の状況で決める
+        del: '削除',
       };
-      // レコード
-      const newRow = elmTblToDo.insertRow();
-      // カラム
-      const newID = newRow.insertCell(0);
-      const newCom = newRow.insertCell(1);
-      const newSta = newRow.insertCell(2);
-      const newDel = newRow.insertCell(3);
-      // データ
-      newID.innerHTML = arrTasks.id;
-      newCom.innerHTML = arrTasks.com;
-      newSta.innerHTML = arrTasks.sta;
-      createBtnDelete(newDel, arrTasks.del);
-      // 入力クリア
-      elmTxtTask.value = '';
       // 入力レコード追加
       allTasks.push(arrTasks);
+      // クリア（空に）
+      elmTxtTask.value = '';
+      elmTblTask.innerHTML = '';
+      // タスクリスト生成
+      const tblBody = elmTblTask;
+      allTasks.forEach(function (rowTask) {
+        const rowBody = tblBody.insertRow();
+        Object.values(rowTask).forEach(function (value) {
+          const tblCell = rowBody.insertCell();
+          if (value == '削除') {
+            createBtn(tblCell, value);
+          } else {
+            tblCell.textContent = value;
+          }
+        });
+      });
+      // リスト追加
+      elmTblToDo.appendChild(tblBody);
     }
   },
   false
@@ -46,12 +51,12 @@ btnAdd.addEventListener(
 
 // ------------------------------
 // 削除ボタン
-const createBtnDelete = (elm, text) => {
-  const btnDelete = document.createElement('button');
-  btnDelete.textContent = text;
-  btnDelete.addEventListener('click', function () {
+const createBtn = (elm, text) => {
+  const elmBtn = document.createElement('button');
+  elmBtn.textContent = text;
+  elmBtn.addEventListener('click', function () {
     // 作業中の処理をここに追加
     console.log('ボタン「削除」がクリックされました。');
   });
-  elm.appendChild(btnDelete);
+  elm.appendChild(elmBtn);
 };
