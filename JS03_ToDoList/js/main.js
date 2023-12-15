30fa823dd70b8e00d0a8824db2abc7a27f4f111d
pinowrk <bourgogne.pino.crawler@gmail.com>
2023-12-16 00:15:40 +0900

    課題完了 2023-12-16 00:15

--- JS03_ToDoList/js/main.js
+++ JS03_ToDoList/js/main.js
@@ -3,13 +3,12 @@ const elmTxtTask = document.getElementById('txtTask');
 const elmTblToDo = document.getElementById('tblToDo');
 const elmTblTask = document.getElementById('tblTask');
 
-//
 // ------------------------------
 // 入力レコード保存用配列
 const allTasks = [];
 
-// イベントリスナー
 // ------------------------------
+// イベントリスナー
 btnAdd.addEventListener(
   'click',
   () => {
@@ -17,33 +16,45 @@ btnAdd.addEventListener(
     const txtTask = elmTxtTask.value.trim();
     // 入力ある場合のみ処理
     if (txtTask !== '') {
-      // 新規レコード用連想（JSON）
-      const arrTasks = {
-        id: allTasks.length,
-        com: txtTask,
-        sta: '作業中',
-      };
-      // 入力レコード追加
-      allTasks.push(arrTasks);
-      // クリア（空に）
+      addAllTasks(txtTask);
+      updTblDatas();
+      // 入力クリア
       elmTxtTask.value = '';
-      elmTblTask.innerHTML = '';
-      // タスクリスト生成
-      const tblBody = elmTblTask;
-      allTasks.forEach(function (rowTask) {
-        const tblRow = tblBody.insertRow();
-        Object.values(rowTask).forEach(function (value) {
-          const tblCell = tblRow.insertCell();
-          tblCell.textContent = value;
-        });
-        createBtn(tblRow, '削除');
-      });
-      // リスト追加
-      elmTblToDo.appendChild(tblBody);
     }
   },
   false
 );
+// ------------------------------
+// レコード保存用配列追加
+const addAllTasks = (txtTask) => {
+  // 新規レコード用オブジェクト
+  const arrTasks = {
+    id: allTasks.length,
+    com: txtTask,
+    sta: '作業中',
+  };
+  // 入力レコード追加
+  allTasks.push(arrTasks);
+};
+// ------------------------------
+// テーブル更新
+const updTblDatas = () => {
+  // テーブルクリア
+  elmTblTask.innerHTML = '';
+  // タスクリスト生成
+  const tblBody = elmTblTask;
+  allTasks.forEach((rowTask, rowIdx) => {
+    rowTask.id = rowIdx;
+    const tblRow = tblBody.insertRow();
+    Object.values(rowTask).forEach((value) => {
+      const tblCell = tblRow.insertCell();
+      tblCell.textContent = value;
+    });
+    createBtn(tblRow, '削除');
+  });
+  // リスト追加
+  elmTblToDo.appendChild(tblBody);
+};
 
 // ------------------------------
 // ボタン作成
@@ -51,9 +62,22 @@ const createBtn = (elm, text) => {
   const elmTrg = elm.insertCell();
   const elmBtn = document.createElement('button');
   elmBtn.textContent = text;
-  elmBtn.addEventListener('click', function (event) {
-    // 作業中の処理をここに追加
-    console.log(event.target.textContent);
+  elmBtn.addEventListener('click', (event) => {
+    const tblRow = event.target.parentNode.parentNode;
+    const arrIdx = tblRow.rowIndex - 1;
+    // 削除ボタンの処理
+    if (event.target.textContent == '削除') {
+      delArrTask(arrIdx);
+    } else {
+      // ここにタスクの状態の処理
+    }
   });
   elmTrg.appendChild(elmBtn);
+};
+
+// ------------------------------
+// タスク削除
+const delArrTask = (arrIdx) => {
+  allTasks.splice(arrIdx, 1);
+  updTblDatas();
 };
